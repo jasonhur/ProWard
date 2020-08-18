7c6bb80ee3474e2c2ce94bfdad0a56cc5f2943c0
Jason Hur <hur.jason14@gmail.com>
2020-08-18 14:53:46 -0700

    Able to plot any comparison within 1 fcn plot_bar()

--- lol_analyze.py
+++ lol_analyze.py
@@ -33,100 +33,41 @@ import numpy as np
 
 def switch(measure):
     switcher = {
-        'KDA': 1,
-        'DPM': 2,
-        'CSPM': 3,
-        'GPM': 4,
-        'Vision': 5
+        'KDA': 0,
+        'DPM': 1,
+        'CSPM': 2,
+        'GPM': 3,
+        'Vision': 4
     }
     return switcher.get(measure, "Invalid measure")
 def plot_bar():
     measure = (str)(input("Choose a measure to observe betweeen the 2 players:\n"\
                       + "{'KDA' 'DPM' 'CSPM' 'GPM' 'Vision'}   "))
     var = switch(measure)
-    print('measure: ' + measure)
-    print('var: ' + str(var))
+    #print('measure: ' + measure)
+    #print('var: ' + str(var))
+    options = [kda, dpm, cspm, gpm, vision]
+    options2 = [kda2, dpm2, cspm2, gpm2, vision2]
+    naming = ['KDA', 'DPM', 'CSPM', 'GPM', 'Vision']
+    if var == 4:
+        avg_option = int(sum(vision)/matchCounter)
+        avg_option2 = int(sum(vision2)/matchCounter)
+    else:
+        avg_option = round(sum(options[var])/matchCounter, 2)
+        avg_option2 = round(sum(options2[var])/matchCounter, 2)
     
-'''	index =np.arange(len(var[i]))
-	width = 0.3
-	index2 = np.arange(len(var[i]))
-	rect1 = plt.bar(index, var[0], width, color = 'b')
-	rect2 = plt.bar(index2 + width, var[1], width, color = 'r')
-	plt.xlabel('games', fontsize = 10)
-	plt.ylabel(measure[i] + ' score', fontsize = 10)
-	plt.title('Avg ' + measure[i] + ' score over ' + str(matchCounter) + ' games')
-    plt.legend( (rect1[0], rect2[0]), (summonerName + ' (' + str(avg_kda) + ')'\
-                                       , summonerName2 + ' (' + str(avg_kda2) + ')') )
-    plt.show()
- '''   
-def plot_bar_kda():
-    index = np.arange(len(kda))
+    index = np.arange(len(options[var]))
     width = 0.3
-    index2 = np.arange(len(kda2))
-    rect1 = plt.bar(index, var, width, color='b')
-    rect2 = plt.bar(index2 + width, kda2, width, color='r')
-    plt.xlabel('games', fontsize=10)
-    plt.ylabel('KDA score', fontsize=10)
-    plt.title('Avg KillDeathAssist Ratio over ' + str(matchCounter) + ' games')
-    plt.legend( (rect1[0], rect2[0]), (summonerName + ' (' + str(avg_kda) + ')'\
-                                       , summonerName2 + ' (' + str(avg_kda2) + ')') )
+    index2 = np.arange(len(options2[var]))
+    rect1 = plt.bar(index, options[var], width, color = 'b')
+    rect2 = plt.bar(index2 + width, options2[var], width, color = 'r')
+    plt.xlabel('games', fontsize = 10)
+    plt.ylabel(naming[var] + ' score', fontsize = 10)
+    plt.title('Avg ' + naming[var] + ' score over ' + str(matchCounter) + ' games')
+    plt.legend( (rect1[0], rect2[0]), (summonerName + ' (' + str(avg_option) + ')'\
+                                       , summonerName2 + ' (' + str(avg_option2) + ')') )
     plt.show()
-
-def plot_bar_dpm():
-    index = np.arange(len(dpm))
-    width = 0.3
-    index2 = np.arange(len(dpm2))
-    rect1 = plt.bar(index, dpm, width, color='b')
-    rect2 = plt.bar(index2 + width, dpm2, width, color='r')
-    plt.xlabel('games', fontsize=10)
-    plt.ylabel('DPM score', fontsize=10)
-    plt.title('Avg DamagePerMin over ' + str(matchCounter) + ' games')
-    plt.legend( (rect1[0], rect2[0]), (summonerName + ' (' + str(avg_dpm) + ')'\
-                                       , summonerName2 + ' (' + str(avg_dpm2) + ')') )
-    plt.show()
-
-def plot_bar_cspm():
-    index = np.arange(len(cspm))
-    width = 0.3
-    index2 = np.arange(len(cspm2))
-    rect1 = plt.bar(index, cspm, width, color='b')
-    rect2 = plt.bar(index2 + width, cspm2, width, color='r')
-    plt.xlabel('games', fontsize=10)
-    plt.ylabel('CSPM score', fontsize=10)
-    plt.title('Avg CreepScorePerMin over ' + str(matchCounter) + ' games')
-    plt.legend( (rect1[0], rect2[0]), (summonerName + ' (' + str(avg_cspm) + ')'\
-                                       , summonerName2 + ' (' + str(avg_cspm2) + ')') )
-    plt.show()
-
-def plot_bar_gpm():
-    index = np.arange(len(gpm))
-    width = 0.3
-    index2 = np.arange(len(gpm2))
-    rect1 = plt.bar(index, gpm, width, color='b')
-    rect2 = plt.bar(index2 + width, gpm2, width, color='r')
-    plt.xlabel('games', fontsize=10)
-    plt.ylabel('GPM score', fontsize=10)
-    plt.title('Avg GoldPerMin over ' + str(matchCounter) + ' games')
-    plt.legend( (rect1[0], rect2[0]), (summonerName + ' (' + str(avg_gpm) + ')'\
-                                       , summonerName2 + ' (' + str(avg_gpm2) + ')') )
-    plt.show()
-
-def plot_bar_vision():
-    index = np.arange(len(vision))
-    width = 0.3
-    index2 = np.arange(len(vision2))
-    rect1 = plt.bar(index, vision, width, color='b')
-    rect2 = plt.bar(index2 + width, vision2, width, color='r')
-    plt.xlabel('games', fontsize=10)
-    plt.ylabel('Vision score', fontsize=10)
-    plt.title('Avg Vision score over ' + str(matchCounter) + ' games')
-    plt.legend( (rect1[0], rect2[0]), (summonerName + ' (' + str(avg_vision) + ')'\
-                                       , summonerName2 + ' (' + str(avg_vision2) + ')') )
-    plt.show()
-
-
-
-
+  
 
 # Get region and user info as input
 region = (str)(input("Type in a region: "))
